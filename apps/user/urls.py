0bf75727c6f2aec58236451ba4a193430c79ba1a
DerekPRINCE <derekmwirigi@gmail.com>
2021-06-27 12:15:39 +0300

    MODIFIED: Rest vies and urls

---
+++ apps/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import *
+
+web_urls = [
+    path('', AuthIndex.as_view(), name='auth'),
+    path('sign-out/', auth_views.LogoutView.as_view(template_name='auth/login.html'), name='sign-out'),
+]

1dc2c09eaff41dd86438908ea2a8459218939758
DerekPRINCE <derekmwirigi@gmail.com>
2021-07-04 21:36:39 +0300

    UPDATED: Added CheckGitHubRespositoryRequestSerializer, Renamed classes

--- apps/repocheck/serializers.py
+++ apps/repocheck/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
 
 
-class GitHubRepository_Serializer(serializers.Serializer):
+class GitHubRepositorySerializer(serializers.Serializer):
     name = serializers.CharField(max_length=250)
     score = serializers.IntegerField(min_value=250)
     popularity = serializers.BooleanField()
 
 
-class CheckGitHubRepositoryReponse_Serializer(serializers.Serializer):
+class CheckGitHubRespositoryRequestSerializer(serializers.Serializer):
+    organisation = serializers.CharField(max_length=32)
+    reposiroty = serializers.CharField(max_length=32)
+
+
+class CheckGitHubRespositoryResponseSerializer(serializers.Serializer):
     status = serializers.BooleanField()
     errors = serializers.ListField(child=serializers.CharField())
     message = serializers.CharField(max_length=32)
-    data = GitHubRepository_Serializer(many=False)
+    data = GitHubRepositorySerializer(many=False)

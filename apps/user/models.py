7d2c9d1c81b1be4bd87e3f5b642b3dcbb216ff7b
DerekPRINCE <derekmwirigi@gmail.com>
2021-07-04 23:32:43 +0300

    UPDATED: Added token

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime, timedelta
+from django.conf import settings
+import jwt
 
 
 class User(AbstractUser):
@@ -20,3 +23,28 @@ class User(AbstractUser):
     @property
     def get_email(self):
         return self.email
+
+    @property
+    def token(self):
+        """
+        Allows us to get a user's token by calling `user.token` instead of
+        `user.generate_jwt_token().
+
+        The `@property` decorator above makes this possible. `token` is called
+        a "dynamic property".
+        """
+        return self._generate_jwt_token()
+
+    def _generate_jwt_token(self):
+        """
+        Generates a JSON Web Token that stores this user's ID and has an expiry
+        date set to 60 days into the future.
+        """
+        dt = datetime.now() + timedelta(days=60)
+
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+        return token.encode().decode('utf-8')
